b74259186b8a2e3f9e0f7bfb44870138f3263aa2
Neil Mitchell <ndmitchell@gmail.com>
2018-01-23 09:53:27 +0000

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,20 +3,26 @@ set -e # exit on errors
 set -x # echo each line
 
 export PATH=$HOME/.cabal/bin:$PATH
-cabal install happy
 ghc --version
 cabal --version
-happy --version
+
+# Currently excluded but should be included
+TODO="ghc-make derive uniplate"
 
 # My list of projects I check on Travis and which might break due to me changing the rules after-the-fact
-PROJECTS="cmdargs debug extra ghc-make ghcid hexml hlint hoogle js-flot js-jquery neil nsis profiterole safe shake tagsoup weeder"
+PROJECTS="cmdargs debug extra ghcid hexml hlint hoogle js-flot js-jquery neil nsis profiterole safe shake tagsoup weeder"
 for PROJECT in ${PROJECTS}; do
     git clone https://github.com/ndmitchell/$PROJECT
 done
 
 (cd neil && cabal install --flags=small)
-(cd hlint && cabal install)
-
 for PROJECT in ${PROJECTS}; do
-    (cd $PROJECT && neil check && hlint .)
+    (cd $PROJECT && neil check)
+done
+
+cabal install happy
+happy --version
+(cd hlint && cabal install)
+for PROJECT in ${PROJECTS}; do
+    (cd $PROJECT && hlint .)
 done
