a35f5bc74d2c6d99a8404294aaf4ade647743a86
Neil Mitchell <ndmitchell@gmail.com>
2018-01-23 09:23:04 +0000

    More improvements

--- travis.sh
+++ travis.sh
@@ -2,8 +2,10 @@
 set -e # exit on errors
 set -x # echo each line
 
+cabal install happy
 ghc --version
 cabal --version
+happy --version
 
 # My list of projects I check on Travis and which might break due to me changing the rules after-the-fact
 PROJECTS="cmdargs debug extra ghc-make ghcid hexml hlint hoogle js-flot js-jquery neil nsis profiterole safe shake tagsoup weeder"
