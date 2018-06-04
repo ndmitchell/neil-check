173c1b39b5f4940c1c39be89d79b75b284cedb5c
Neil Mitchell <ndmitchell@gmail.com>
2018-06-04 13:56:00 +0100

    Refactor to try each stage against every test

--- Main.hs
+++ Main.hs
@@ -2,6 +2,8 @@
 import System.IO.Extra
 import System.Directory.Extra
 import Data.List
+import Data.Maybe
+import Control.Exception.Extra
 import Control.Monad
 import System.Process.Extra
 import System.FilePath
@@ -11,6 +13,17 @@ projects =
     -- deliberately exclude ghc-make, derive, uniplate
     map ("ndmitchell/" ++) (words "cmdargs debug extra ghcid hexml hlint hoogle js-flot js-jquery neil nsis profiterole safe shake tagsoup weeder") ++
     ["haskell/filepath"]
+
+forEachProject :: (String -> IO ()) -> IO ()
+forEachProject act = do
+    failed <- fmap catMaybes $ forM projects $ \p -> do
+        res <- try_ $ act p
+        case res of
+            Left e -> do putStrLn $ "FAILED: " ++ p ++ ", " ++ show e; return $ Just p
+            Right _ -> return Nothing
+    when (failed /= []) $
+        putStrLn $ "FAILED: " ++ unwords failed
+
 
 main = withTempDir $ \tdir -> withCurrentDirectory tdir $ do
     system_ <- return $ \x -> do
@@ -25,7 +38,7 @@ main = withTempDir $ \tdir -> withCurrentDirectory tdir $ do
         system_ "cabal install --dependencies"
         system_ "cabal configure --flags=small"
         system_ "cabal build"
-    forM_ projects $ \p ->
+    forEachProject $ \p ->
         withCurrentDirectory (takeFileName p) $ system_ $ normalise "../neil/dist/build/neil/neil" ++ " check"
 
     withCurrentDirectory "hlint" $ do
@@ -39,7 +52,7 @@ main = withTempDir $ \tdir -> withCurrentDirectory tdir $ do
             let out = "dist/build/hlint" </> file
             createDirectoryIfMissing True $ takeDirectory out
             copyFile file out
-    forM_ projects $ \p ->
+    forEachProject $ \p ->
         withCurrentDirectory (takeFileName p) $ do
             b <- doesDirectoryExist "src"
             system_ $ normalise "../hlint/dist/build/hlint/hlint" ++ " " ++ (if b then "src" else ".")
